3b3366a8bc0d26d21d982b10ac618a700cbb70f2
Anton Morgunov <morgunov@mit.edu>
2023-10-22 19:36:50 -0400

    STYLE: types for sampling

---
+++ ChemSpaceAL/README.md
@@ -0,0 +1,5 @@
+# ChemSpaceAL internals
+
+## TO-DO
+
+- [ ] fix the types in `Sampling.py` for `cluster_to_samples: Dict[int, Any] = {}`
--- ChemSpaceAL/Sampling.py
+++ ChemSpaceAL/Sampling.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 from ChemSpaceAL.Configuration import Config
-from typing import Optional, Dict, List, Any
+from typing import Optional, Dict, List, Any, Union, Iterable, Sized
 
 
 def calculate_descriptors(config: Config, smiles_fname: Optional[str] = None):
@@ -59,7 +59,7 @@ def calculate_descriptors(config: Config, smiles_fname: Optional[str] = None):
     gpt_df.to_pickle(config.cycle_temp_params["path_to_descriptors"])
 
 
-def project_into_pca_space(config):
+def project_into_pca_space(config: Config) -> np.ndarray:
     """Project molecules into PCA space.
 
     Args:
@@ -68,14 +68,17 @@ def project_into_pca_space(config):
     Returns:
         np.array: Array of PCA transformed molecules.
     """
-    scaler, pca = pickle.load(open(config["path_to_pca"], "rb"))
-    gptMols = pd.read_pickle(config["path_to_gen_mol_descriptors"])
+    assert (
+        config.cycle_temp_params["path_to_pca"] is not None
+    ), "please provide path_to_pca through .set_sampling_parameters"
+    scaler, pca = pickle.load(open(config.cycle_temp_params["path_to_pca"], "rb"))
+    gptMols = pd.read_pickle(config.cycle_temp_params["path_to_descriptors"])
     return pca.transform(scaler.transform(gptMols[scaler.get_feature_names_out()]))
 
 
-def _cluster_mols_experimental_mixed(
-    mols, n_clusters, n_iter, mixed_objective_loss_quantile
-):
+def _cluster_mols_by_mixed(
+    mols: np.ndarray, n_clusters: int, n_iter: int, mixed_objective_loss_quantile: float
+) -> KMeans:
     """Cluster molecules using experimental mixed method.
 
     Args:
@@ -95,10 +98,8 @@ def _cluster_mols_experimental_mixed(
             mols
         )
         inertias.append(kmeans.inertia_)
-
         counts = np.unique(kmeans.labels_, return_counts=True)[1]
         variances.append(np.var(counts))
-
         km_objs.append(kmeans)
 
     loss_var_kmeans_triples = sorted(
@@ -111,43 +112,65 @@ def _cluster_mols_experimental_mixed(
     return sorted_by_variance[0][2]
 
 
-def _cluster_mols_experimental(
-    mols,
-    n_clusters,
-    save_path,
-    n_iter=1,
-    objective="loss",
+def _cluster_mols_by_loss(mols: np.ndarray, n_clusters: int, n_iter: int) -> KMeans:
+    min_loss, best_kmeans = float("inf"), None
+    for _ in tqdm(range(n_iter), total=n_iter):
+        kmeans = KMeans(n_clusters=n_clusters, n_init="auto", init="k-means++").fit(
+            mols
+        )
+        if kmeans.inertia_ < min_loss:
+            min_loss = kmeans.inertia_
+            best_kmeans = kmeans
+    return best_kmeans
+
+
+def _cluster_mols_by_variance(mols: np.ndarray, n_clusters: int, n_iter: int) -> KMeans:
+    max_variance, best_kmeans = float("-inf"), None
+    for _ in tqdm(range(n_iter), total=n_iter):
+        kmeans = KMeans(n_clusters=n_clusters, n_init="auto", init="k-means++").fit(
+            mols
+        )
+        counts = np.unique(kmeans.labels_, return_counts=True)[1]
+        if (variance := np.var(counts)) > max_variance:
+            max_variance = variance  # type: ignore
+            best_kmeans = kmeans
+    return best_kmeans
+
+
+def _cluster_mols(
+    mols: np.ndarray,
+    n_clusters: int,
+    save_path: str,
+    n_iter: int = 1,
+    objective: str = "loss",
     mixed_objective_loss_quantile=0.1,
-):
-    """Cluster molecules using experimental method and save the KMeans model.
+) -> KMeans:
+    if n_iter == 1:
+        kmeans = KMeans(n_clusters=n_clusters, n_init="auto", init="k-means++").fit(
+            mols
+        )
+    elif objective == "loss":
+        kmeans = _cluster_mols_by_loss(mols, n_clusters, n_iter)
+    elif objective == "variance":
+        kmeans = _cluster_mols_by_variance(mols, n_clusters, n_iter)
+    elif objective == "mixed":
+        kmeans = _cluster_mols_by_mixed(
+            mols, n_clusters, n_iter, mixed_objective_loss_quantile
+        )
+    else:
+        raise ValueError(f"Unknown objective {objective}")
 
-    Args:
-        mols (np.array): Array of molecules.
-        n_clusters (int): Number of clusters.
-        save_path (str): Path to save KMeans model.
-        n_iter (int, optional): Number of iterations. Defaults to 1.
-        objective (str, optional): Objective metric. Defaults to 'loss'.
-        mixed_objective_loss_quantile (float, optional): Quantile for loss. Defaults to 0.1.
-
-    Returns:
-        object: Fitted KMeans clustering object.
-    """
-    kmeans = _cluster_mols_experimental_mixed(
-        mols, n_clusters, n_iter, mixed_objective_loss_quantile
-    )
     pickle.dump(kmeans, open(save_path, "wb"))
     return kmeans
 
 
 def cluster_and_sample(
-    mols,
-    config,
-    n_clusters,
-    n_samples,
-    ensure_correctness=False,
-    path_to_pca=None,
+    mols: np.ndarray,
+    config: Config,
+    n_iter: int = 100,
+    objective: str = "mixed",
     load_kmeans=False,
-):
+) -> Dict[int, Any]:
     """Cluster the molecules and then sample from each cluster.
 
     Args:
@@ -164,71 +187,75 @@ def cluster_and_sample(
     """
 
     # Assert checks to make sure input parameters are valid
-    assert n_clusters * n_samples <= len(
+    not_init_error = ".set_sampling_parameters() was not called"
+    assert isinstance(
+        (samples_per_cluster := config.sampling_parameters["samples_per_cluster"]), int
+    ), not_init_error
+    assert isinstance(
+        (n_samples := config.sampling_parameters["n_samples"]), int
+    ), not_init_error
+    assert isinstance(
+        (n_clusters := config.sampling_parameters["n_clusters"]), int
+    ), not_init_error
+    path_to_pca = config.cycle_temp_params["path_to_pca"]
+    assert n_samples <= len(
         mols
-    ), f"{n_clusters=} * {n_samples=} = {n_clusters*n_samples} requested but only {len(mols)} molecules provided"
+    ), f"{n_samples} requested but only {len(mols)} molecules provided"
 
-    if ensure_correctness:
-        assert (
-            path_to_pca is not None
-        ), "path_to_pca must be provided to ensure correctness"
-        scaler, pca = pickle.load(open(path_to_pca, "rb"))
-
+    assert isinstance(
+        (kmeans_path := config.cycle_temp_params["path_to_kmeans"]), str
+    ), not_init_error
     if load_kmeans:
-        kmeans = pickle.load(open(config["kmeans_save_path"], "rb"))
+        kmeans = pickle.load(open(kmeans_path, "rb"))
     else:
-        kmeans = _cluster_mols_experimental(
+        kmeans = _cluster_mols(
             mols=mols,
-            n_iter=100,
+            n_iter=n_iter,
             n_clusters=n_clusters,
-            save_path=config["kmeans_save_path"],
-            objective="mixed",
+            save_path=kmeans_path,
+            objective=objective,
             mixed_objective_loss_quantile=0.05,
         )
 
     # Load SMILES strings
-    mols_smiles = pd.read_pickle(config["path_to_gen_mol_descriptors"])["smiles"]
+    assert isinstance(
+        (desc_path := config.cycle_temp_params["path_to_descriptors"]), str
+    ), not_init_error
+    mols_smiles = pd.read_pickle(desc_path)["smiles"]
     assert len(kmeans.labels_) == len(
         mols_smiles
     ), "Number of labels differs from number of molecules"
 
     # Load previously scored molecules
     scored_smiles = set()
-    for scored_file in config["diffdock_scored_path_list"]:
+    assert isinstance(
+        config.previously_scored_mols, list
+    ), ".set_previous_arrays() wasn't called"
+    for scored_file in config.previously_scored_mols:
         for smile in pd.read_csv(scored_file)["smiles"].values:
             scored_smiles.add(smile)
     print(f"Loaded {len(scored_smiles)} scored molecules")
 
     # Collect non-repeated molecules from clusters
     repeated = 0
-    cluster_to_mols = {}
+    cluster_to_mols: Dict[int, List[str]] = {}
     for mol, label, smile in zip(mols, kmeans.labels_, mols_smiles):
         if smile in scored_smiles:
             repeated += 1
             continue
         cluster_to_mols.setdefault(label, []).append(smile)
-        if ensure_correctness:
-            smile_features = pca.transform(
-                scaler.transform(
-                    pd.DataFrame(
-                        {
-                            k: [v]
-                            for k, v in rdkit.Chem.Descriptors.CalcMolDescriptors(
-                                rdkit.Chem.MolFromSmiles(smile)
-                            ).items()
-                        }
-                    )[scaler.get_feature_names_out()]
-                )
-            )
-            assert np.allclose(
-                smile_features[0], mol
-            ), "Features calculated from a smile string differ from features in the array"
-
-    # Distribute extra molecules across clusters proportionally
+    print(
+        f"{repeated} generated molecules were already scored. Excluding them from sampling."
+    )
+    # What happens below is sampling from each cluster. All the extra code is to ensure that the number of samples requested from each cluster
+    # doesn't exceed the total number of available molecules. This is done by calculating the average number of molecules per cluster and then
+    # calculating the number of extra molecules that need to be sampled from each cluster. The extra molecules are then distributed among the
+    # clusters uniformly. If the number of extra molecules is greater than the number of molecules in a cluster, all
+    # molecules from that cluster are sampled.
     avg_len = np.mean([len(v) for v in cluster_to_mols.values()])
-    cluster_to_samples = {}
-    extra_mols = (100 - len(cluster_to_mols)) * 10
-    left_to_sample = n_clusters * n_samples
+    cluster_to_samples: Dict[int, Any] = {}
+    extra_mols = (n_clusters - len(cluster_to_mols)) * samples_per_cluster
+    left_to_sample = n_samples
     cluster_to_len = {cluster: len(mols) for cluster, mols in cluster_to_mols.items()}
 
     for i, (cluster, _) in enumerate(
@@ -239,10 +266,11 @@ def cluster_and_sample(
             cur_extra = int(
                 1 + extra_mols / (len(cluster_to_mols) - i) * len(smiles) / avg_len
             )
-            cur_samples = n_samples + cur_extra
+            cur_samples = samples_per_cluster + cur_extra
             extra_mols -= cur_extra
         else:
-            cur_samples = n_samples
+            cur_samples = samples_per_cluster
+        # print(f"{cluster=}, {left_to_sample=}, {cur_samples=}")
         if cur_samples > left_to_sample:
             cur_samples = left_to_sample
         if len(smiles) > cur_samples:
@@ -257,20 +285,23 @@ def cluster_and_sample(
 
     assert (
         n_sampled := sum(len(vals) for vals in cluster_to_samples.values())
-    ) == n_clusters * n_samples, (
-        f"Sampled {n_sampled} but were requested {n_clusters*n_samples}"
-    )
+    ) == n_samples, f"Sampled {n_sampled} but were requested {n_samples}"
 
     # Save clusters and samples
-    pickle.dump(cluster_to_mols, open(config["clusters_save_path"], "wb"))
-    pickle.dump(cluster_to_samples, open(config["samples_save_path"], "wb"))
+    assert isinstance(
+        (clusters_path := config.cycle_temp_params["path_to_clusters"]), str
+    ), not_init_error
+    pickle.dump(cluster_to_mols, open(clusters_path, "wb"))
 
+    assert isinstance(
+        (samples_path := config.cycle_temp_params["path_to_sampled"]), str
+    ), not_init_error
     # Prepare the data for saving in csv format
-    keyToData = {}
+    keyToData: Dict[str, List[Union[str, int]]] = {}
     for cluster, mols in cluster_to_samples.items():
         for mol in mols:
             keyToData.setdefault("smiles", []).append(mol)
             keyToData.setdefault("cluster_id", []).append(cluster)
-    pd.DataFrame(keyToData).to_csv(config["diffdock_save_path"])
+    pd.DataFrame(keyToData).to_csv(samples_path)
 
     return cluster_to_samples
