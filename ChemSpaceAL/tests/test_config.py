f1306ee895e2d289f756e515725cfb23afbc2113
Anton Morgunov <morgunov@mit.edu>
2023-10-24 16:21:34 -0400

    TESTS: check folder creation

--- ChemSpaceAL/tests/test_config.py
+++
@@ -1,7 +0,0 @@
-import ChemSpaceAL.InitializeWorkspace as iw
-import os
-
-def test_config():
-    base_path = os.getcwd() + "/PaperRuns/"
-    assert isinstance(iw.FOLDER_STRUCTURE, dict)
-    assert True
---
+++ ChemSpaceAL/tests/test_initialize_workspace.py
@@ -0,0 +1,18 @@
+import os
+import tempfile
+from unittest.mock import patch
+import ChemSpaceAL.InitializeWorkspace as iw
+
+
+def test_create_default_folders():
+    # Using a temporary directory for testing
+    with tempfile.TemporaryDirectory() as tempdir:
+        iw.create_folders(base_path=tempdir)
+
+        # Check if the main folders are created
+        assert os.path.exists(os.path.join(tempdir, "1_Pretraining"))
+        assert os.path.exists(os.path.join(tempdir, "2_Generation"))
+
+        # Check if subfolders are created
+        assert os.path.exists(os.path.join(tempdir, "1_Pretraining", "datasets"))
+        # ... (add more assertions for other subfolders)
