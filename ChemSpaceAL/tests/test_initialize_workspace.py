4b9c2e1f85012741355f1b52e96f8e4132dcc459
Anton Morgunov <morgunov@mit.edu>
2023-10-24 16:55:17 -0400

    TESTS: add more checks

--- ChemSpaceAL/tests/test_initialize_workspace.py
+++ ChemSpaceAL/tests/test_initialize_workspace.py
@@ -16,3 +16,11 @@ def test_create_default_folders():
         # Check if subfolders are created
         assert os.path.exists(os.path.join(tempdir, "1_Pretraining", "datasets"))
         # ... (add more assertions for other subfolders)
+    assert isinstance(iw.FOLDER_STRUCTURE, dict)
+
+
+@patch("builtins.input", return_value="Y")
+def test_create_folders_without_base_path(mock_input):
+    with tempfile.TemporaryDirectory() as tempdir:
+        os.chdir(tempdir)  # Change working directory
+        iw.create_folders()
