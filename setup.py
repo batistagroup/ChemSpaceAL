f3f3145b5993612ba950b8e06dd27ec667a89d38
Anton Morgunov <anton@ischemist.com>
2024-07-22 15:50:13 -0600

    DEV: downgrade numpy, upgrade torch in reqs

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "pandas==1.5.3",
         "numpy==1.25.2",
         "rdkit==2023.03.3",
-        "torch==2.0.1",
+        "torch==2.1.0",
         "PyYAML",
         "scikit_learn",
         "tqdm",
